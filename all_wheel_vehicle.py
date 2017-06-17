65850a978b3afa5bca8d2ffae0955d10c21719df
Jorgen Miller <wookieintheyard@gmail.com>
2017-06-17 14:58:07 -0400

    all wheel vehicle

---
+++ all_wheel_vehicle.py
@@ -0,0 +1,33 @@
+from inputs import get_gamepad
+from BrickPi import *
+import threading
+
+BrickPiSetup()
+BrickPi.MotorEnable[PORT_A] = 1
+BrickPi.MotorEnable[PORT_B] = 1
+BrickPi.MotorEnable[PORT_C] = 1
+BrickPi.MotorEnable[PORT_D] = 1
+
+quitting = False
+
+def gamepad():
+    while True:
+        for event in get_gamepad():
+            if event.code == "ABS_Y":
+                BrickPi.MotorSpeed[PORT_A] = event.state / -128
+                BrickPi.MotorSpeed[PORT_B] = event.state / 128
+            elif event.code == "ABS_RY":
+                BrickPi.MotorSpeed[PORT_C] = event.state / 128
+                BrickPi.MotorSpeed[PORT_D] = event.state / -128
+            elif event.code == "BTN_MODE":
+                global quitting
+                quitting = True
+                return
+
+gamepad = threading.Thread(target = gamepad)
+gamepad.start()
+
+while True:
+    BrickPiUpdateValues()
+    if quitting == True:
+        sys.exit()
