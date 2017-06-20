0017a1406456e028ecf2f6c82ccdbcec4943a1ca
Jorgen Miller <wookieintheyard@gmail.com>
2017-06-20 11:38:11 -0400

    servo test

--- encoder_test.py
+++ encoder_test.py
@@ -4,9 +4,9 @@ BrickPiSetup()
 BrickPiSetupSensors()
 BrickPi.MotorEnable[PORT_A] = 1
 
+BrickPi.offset_motor_encoder = BrickPi.encoder[PORT_A]
 motorRotateDegree([25],[0],[PORT_A],0)
 print "0"
-time.sleep(1)
 motorRotateDegree([200],[90],[PORT_A],0)
 print "255"
 
