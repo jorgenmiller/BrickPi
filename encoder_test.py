79e4307b3a327391c18128588364a9cb6be69a66
Jorgen Miller <wookieintheyard@gmail.com>
2017-06-20 11:40:21 -0400

    encoder

--- encoder_test.py
+++ encoder_test.py
@@ -7,7 +7,7 @@ BrickPi.MotorEnable[PORT_A] = 1
 BrickPi.offset_motor_encoder = BrickPi.Encoder[PORT_A]
 motorRotateDegree([25],[0],[PORT_A],0)
 print "0"
-motorRotateDegree([200],[90],[PORT_A],0)
+motorRotateDegree([25],[360],[PORT_A],0)
 print "255"
 
 while True:
