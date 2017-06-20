d1384c33e56b10f96f0f453bebf9daacc2612eeb
Jorgen Miller <wookieintheyard@gmail.com>
2017-06-20 10:15:19 -0400

    motor encoder

--- encoder_test.py
+++ encoder_test.py
@@ -5,7 +5,6 @@ BrickPiSetupSensors()
 BrickPi.MotorEnable[PORT_A] = 1
 
 while True:
-    pos = BrickPi.Encoder[PORT_A]
-    print pos / 2
+    print BrickPi.Encoder[PORT_A]
     BrickPiUpdateValues()
     time.sleep(.1)
