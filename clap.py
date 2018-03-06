d844f68cdbf34344cd31b0490a37ffa142c1fb97
jorgenmiller <jorgen.j.miller@gmail.com>
2018-03-06 15:57:46 -0500

    clap detector

--- clap.py
+++ clap.py
@@ -4,11 +4,12 @@ BrickPiSetup()
 BrickPi.SensorType[PORT_1] = TYPE_SENSOR_RAW #analog sound sensor
 BrickPiSetupSensors()
 
+max = 1000
+
 while True:
     try:
         BrickPiUpdateValues()
         soundlevel = BrickPi.Sensor[PORT_1]
-        max = 1000
         if soundlevel < max:
             max = soundlevel
             print max #print the output (1000-quiet, 0-loud)
