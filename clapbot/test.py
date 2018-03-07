0e94ad21397fd53dde879d9f77f9ee56cd0c66dd
jorgenmiller <jorgen.j.miller@gmail.com>
2018-03-07 18:17:35 -0500

    clap detector

--- clapbot/test.py
+++ clapbot/test.py
@@ -6,15 +6,13 @@ BrickPi.SensorType[PORT_1] = TYPE_SENSOR_RAW #analog sound sensor
 BrickPiSetupSensors()
 
 def good(soundlevel):
-    return ( 1 - ( soundlevel / 1000 ) )
+    return ( 1 - ( float(soundlevel) / 1000 ) )
 
 while True:
     try:
 
         BrickPiUpdateValues()
-        print BrickPi.Sensor[PORT_1]
         print good(BrickPi.Sensor[PORT_1])
-        print ( 1 - ( float(BrickPi.Sensor[PORT_1]) / 1000 ) )
 
     except KeyboardInterrupt:
         break
