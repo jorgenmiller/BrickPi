22ddb4cb4ffed84edb8023f5c3422c4ef924b2e9
jorgenmiller <jorgen.j.miller@gmail.com>
2018-03-22 15:51:21 -0400

    science fair

--- ScienceFair/arms.py
+++ ScienceFair/arms.py
@@ -20,18 +20,24 @@ while True:
     try:
         goto = input("pos? 0-360: ")
         position = BrickPi.Encoder[PORT_A]
+        print("got it")
 
         if goto > position:
             BrickPi.MotorSpeed[PORT_A] = 50
+            print("up")
         elif goto < position:
             BrickPi.MotorSpeed[PORT_A] = -50
+            print("down")
         else:
             BrickPi.MotorSpeed[PORT_A] = 0
+            print("stay")
 
             while position < goto - 30 or position > goto + 30:
+                print("again")
                 BrickPiUpdateValues()
         BrickPi.MotorSpeed[PORT_A] = 0
         BrickPiUpdateValues()
+        print("finished")
 
     except KeyboardInterrupt:
         break
